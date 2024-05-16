d5f34ac5a6538720a1f13fa0db3de9b04c155bcb
twentyfivesrl <aurelius99w@hotmail.it>
2024-05-16 13:41:29 +0200

    aggiustato torna indietro ingredients

--- src/app/modules-client/catalog/catalog-routing.module.ts
+++ src/app/modules-client/catalog/catalog-routing.module.ts
@@ -1,13 +1,25 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {CatalogComponent} from "./components/catalog/catalog.component";
+import {UserProfileComponent} from "./components/pages/user-profile/user-profile.component";
+import {UserEditComponent} from "./components/pages/user-edit/user-edit.component";
 
 const routes: Routes = [
-  {path: '', component: CatalogComponent},
+  {path: '', redirectTo: 'profilo', pathMatch: 'full'},
+  {
+    path: '',
+    component: CatalogComponent,
+    children: [
+      {path:'profilo',component:UserProfileComponent, data: {title: 'Profilo'}},
+      {path:'userEdit/:id',component:UserEditComponent, data: {title: 'ModificaProfilo'}},
+
+    ]
+  }
 ]
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
 export class CatalogRoutingModule {
+
 }
--- src/app/modules-client/catalog/catalog.module.ts
+++ src/app/modules-client/catalog/catalog.module.ts
@@ -2,10 +2,14 @@ import {NgModule} from "@angular/core";
 import {SharedModule} from "../../shared/shared.module";
 import {CatalogComponent} from "./components/catalog/catalog.component";
 import {CatalogRoutingModule} from "./catalog-routing.module";
+import { UserProfileComponent } from './components/pages/user-profile/user-profile.component';
+import { UserEditComponent } from './components/pages/user-edit/user-edit.component';
 
 @NgModule({
   declarations: [
     CatalogComponent,
+    UserProfileComponent,
+    UserEditComponent,
 
   ],
   imports: [
