85e717c4473600b68c4ed24f5a5c2177e9056a39
loscls <lorenzo.j.scalise@gmail.com>
2024-05-20 13:05:06 +0200

    add user prfile, user edit, user order

--- src/app/modules-admin/dashboard/pages/order-list/order-list.component.ts
+++ src/app/modules-admin/dashboard/pages/order-list/order-list.component.ts
@@ -112,7 +112,7 @@ export class OrderListComponent implements OnInit, AfterViewInit{
       showFunction: () => {
         return true;
       }
-    },
+    }
   ]
 
   constructor( private router: Router,
--- src/app/modules-client/dashboard-client/dashboard-client.module.ts
+++ src/app/modules-client/dashboard-client/dashboard-client.module.ts
@@ -4,11 +4,17 @@ import {DashboardClient} from "./components/dashboard-client/dashboard-client";
 import {DashboardClientRoutingModule} from "./dashboard-client-routing.module";
 import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from "@ng-bootstrap/ng-bootstrap";
 import {CatalogueComponent} from "./pages/catalogue/catalogue.component";
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import {UserEditComponent} from "./pages/user-edit/user-edit.component";
+import { UserOrderComponent } from './pages/user-order/user-order.component';
 
 @NgModule({
   declarations: [
     DashboardClient,
     CatalogueComponent,
+    UserProfileComponent,
+    UserEditComponent,
+    UserOrderComponent,
   ],
   imports: [
     DashboardClientRoutingModule,
