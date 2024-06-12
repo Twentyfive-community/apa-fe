85374e7c1577932e9df2cc2ac144023b309431bc
Raikozu <francescogaglianese93@gmail.com>
2024-06-12 22:54:32 +0200

    - Spinner in every pages! yay!

--- src/app/shared/apa-spinner/apa-spinner.component.ts
+++ src/app/shared/apa-spinner/apa-spinner.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'apa-spinner',
   templateUrl: './apa-spinner.component.html',
   styleUrl: './apa-spinner.component.scss'
 })
-export class ApaSpinnerComponent {
+export class ApaSpinnerComponent implements OnChanges{
+  ngOnChanges() {
+      const spinnerElement = document.querySelector('.spinner-overlay');
+      if (spinnerElement) {
+        setTimeout(() => {
+          spinnerElement.classList.add('hidden');
+        }, 2000); // Spinner will fade away after 2 seconds
+      }
+  }
 
 }
