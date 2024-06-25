50714fc2d5218e88a067251fad82835ee2da3d72
Raikozu <francescogaglianese93@gmail.com>
2024-06-25 13:00:19 +0200

    - Now allergens are displayed in catalogue and tray customized

--- src/app/shared/generic-card/generic-card.component.ts
+++ src/app/shared/generic-card/generic-card.component.ts
@@ -17,7 +17,7 @@ export class GenericCardComponent implements OnInit {
   @Input() currentWeight?: number;
   @Input() totalWeight?: number;
   @Output() resultChange = new EventEmitter<number>();
-  @Input() allergens: string[];
+  @Input() allergens: Allergen[];
   @Input() description?: string;
   @Input() price: string;
 
