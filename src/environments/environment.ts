df04be06461d4a8dfb82d952776d87262ae1a841
Raikozu <francescogaglianese93@gmail.com>
2024-07-01 13:41:09 +0200

    -Fixed Environments for starting v2.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
 
   keycloakurl: 'http://80.211.123.141:9001',
-  realmname: 'Antica-Pasticceria-v2',
+  realmname: 'DEV',
   clientid: 'apa-app',
   redirecturi: '',
   backendUrl: 'http://localhost:8080',
