8df27548519436294b3e007f39357ca6d192e482
loscls <lorenzo.j.scalise@gmail.com>
2024-07-12 16:15:08 +0200

    menu model

--- src/app/models/Menu.ts
+++ src/app/models/Menu.ts
@@ -1,9 +1,18 @@
+import {Allergen} from "./Allergen";
+
 export class MenuSection {
     id:string;
     name:string;
     active:boolean = true;
 }
 export class MenuItem {
+  id: string;
+  name: string;
+  categoryId: string;
+  description: string;
+  price: number;
+  allergens: Allergen[] ;
+  imageUrl: string;
 }
 
 export class MenuItemDetails {
