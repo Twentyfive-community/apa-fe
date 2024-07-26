cefc584957d59abfd6f4e44df516eaf8b048fa4d
Raikozu <francescogaglianese93@gmail.com>
2024-07-26 17:35:42 +0200

    fixed (maybe) type of the product in card cart

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,12 @@ export const environment = {
   cookieKey: 'cookieResponse',
 
   ftpUrl: 'http://80.211.123.141:8106/TwentyfiveMediaManager/twentyfiveserver',
-  ftpDownloadUrl: 'http://80.211.123.141:8106/TwentyfiveMediaManager/twentyfiveserver/downloadkkk/apa/products/'
+  ftpDownloadUrl: 'http://80.211.123.141:8106/TwentyfiveMediaManager/twentyfiveserver/downloadkkk/apa/products/',
+
+  //payment things.
+  currency:'EUR',
+  cancelUrl:'http:localhost:4200/catalogo/carrello',
+  returnUrl:'http:localhost:4200/catalogo/carrello',
+  kafkaTopic:'twentyfive_dev_preorders',
+  paymentUrl:'http://80.211.123.141:8106/groypal-deamon',
 };
